ca084736c710cfd772d3dca1e08d7bd337c0d8bd
Robosword <robotanmol@gmail.com>
2026-08-17 16:18:48 -0700

    Add Page and PageHandler classes for LCD screen

--- lcdScreen.py
+++ lcdScreen.py
@@ -1,1 +1,22 @@
+from gpiozero import Button
 
+class Page:
+  def __init__(self, carName, carBrand, time):
+    self.carName = carName
+    self.carBrand = carBrand
+    self.time = time
+
+  def display(self):
+    pass
+  
+class PageHandler:
+  def __init__(self):
+    self.pages = []
+
+  def update():
+    pass
+
+  def switch():
+    pass
+
+    
